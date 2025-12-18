86a4e28957eef7a0b2a9f8058db1585726f4e102
LewdM3at <angkunster@hotmail.com>
2025-12-18 16:24:22 +0100

    added clearscreens for actions

--- actions.py
+++ actions.py
@@ -4,18 +4,22 @@ import sys
 
 def pmkid_attack():
     curses.endwin()
+    print("\033c", end="")  # full terminal reset
     subprocess.run(["sudo", "wifite", "--pmkid", "--daemon"])
 
 def handshake_attack():
     curses.endwin()
+    print("\033c", end="")  # full terminal reset
     subprocess.run(["sudo", "wifite", "--no-pmkid", "--no-wps", "--daemon"])
 
 def pixiedust_attack():
     curses.endwin()
+    print("\033c", end="")  # full terminal reset
     subprocess.run(["sudo", "wifite", "--pixie", "--no-pmkid", "--wps-only", "--daemon"])
 
 def pin_attack():
     curses.endwin()
+    print("\033c", end="")  # full terminal reset
     subprocess.run(["sudo", "wifite", "--no-pixie", "--no-pmkid", "--wps-only", "--daemon"])
 
 def exit_menu():
