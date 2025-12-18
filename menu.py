ba5edcc0816a831f52d65652d7e7dce158be2b82
LewdM3at <angkunster@hotmail.com>
2025-12-18 10:37:51 +0100

    refactored menu.py

--- menu.py
+++ menu.py
@@ -15,18 +15,25 @@ def draw_ascii_and_title(stdscr, art, title):
 
     # Draw art
     for i, line in enumerate(art):
-        stdscr.addstr(y_offset + i, x_offset, line)
+        try:
+            stdscr.addstr(y_offset + i, x_offset, line)
+        except curses.error:
+            pass  # ignore if drawing goes out of bounds
 
     # Draw title below art, centered
-    stdscr.addstr(y_offset + art_height + 1,
-                  (w // 2) - (len(title) // 2),
-                  title)
+    try:
+        stdscr.addstr(y_offset + art_height + 1,
+                      (w // 2) - (len(title) // 2),
+                      title)
+    except curses.error:
+        pass
 
 
 def run_menu(stdscr, start_menu="main"):
     curses.curs_set(0)
     current_menu = start_menu
     selected_idx = 0
+    menu_stack = [current_menu]
 
     while True:
         stdscr.clear()
@@ -50,22 +57,19 @@ def run_menu(stdscr, start_menu="main"):
         elif key in [curses.KEY_ENTER, 10, 13]:
             # Handle actions or submenu navigation
             if "action" in item:
-                curses.endwin()  # restore terminal before running action
+                curses.endwin()
                 item["action"]()
-                # after action returns, reinitialize curses
+                # If action returns, reinitialize curses
                 stdscr = curses.initscr()
                 curses.curs_set(0)
             elif "submenu" in item:
+                menu_stack.append(item["submenu"])
                 current_menu = item["submenu"]
                 selected_idx = 0
             elif item_key == "back":
-                # go back to parent menu if defined
-                if "parent" in menu_structure[current_menu]:
-                    current_menu = menu_structure[current_menu]["parent"]
+                if len(menu_stack) > 1:
+                    menu_stack.pop()
+                    current_menu = menu_stack[-1]
                     selected_idx = 0
         elif key == 27:  # ESC key
             break
-
-
-if __name__ == "__main__":
-    curses.wrapper(run_menu)
